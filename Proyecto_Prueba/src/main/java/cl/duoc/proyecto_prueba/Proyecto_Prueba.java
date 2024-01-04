6f095d04c660a3b5a598821979b96967eda100fd
FelipeMillanOrellana <107309552+FelipeMillanOrellana@users.noreply.github.com>
2024-01-03 22:05:32 -0300

    Add files via upload

---
+++ Proyecto_Prueba/src/main/java/cl/duoc/proyecto_prueba/Proyecto_Prueba.java
@@ -0,0 +1,10 @@
+package cl.duoc.proyecto_prueba;
+
+public class Proyecto_Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+    }
+}
