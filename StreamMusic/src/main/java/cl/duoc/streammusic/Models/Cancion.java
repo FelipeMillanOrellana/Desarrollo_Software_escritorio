c16e435db69ce8644048efc250d82449bac964c9
FelipeMillanOrellana <107309552+FelipeMillanOrellana@users.noreply.github.com>
2024-01-05 21:32:04 -0300

    Update Cancion.java

--- StreamMusic/src/main/java/cl/duoc/streammusic/Models/Cancion.java
+++ StreamMusic/src/main/java/cl/duoc/streammusic/Models/Cancion.java
@@ -3,30 +3,43 @@ package cl.duoc.streammusic.Models;
 import java.time.LocalTime;
 
 public class Cancion {
-    public String titulo;
-    public String artista;
-    public LocalTime duracion;
+    
+    private int id;
+    private String titulo;
+    private String artista;
+    private LocalTime duracion;
     private boolean favorita;
     private boolean descargada;
-
-    public Cancion(String titulo, String artista, LocalTime duracion, boolean favorita, boolean descargada) {
+//Constructor
+    public Cancion(int id,String titulo, String artista, LocalTime duracion, boolean favorita, boolean descargada) {
+        this.id = id;
         this.titulo = titulo;
         this.artista = artista;
         this.duracion = duracion;
         this.favorita = favorita;
         this.descargada = descargada;
     }
-
+//Constructor vacio
     public Cancion() {
-        this.titulo = "";
-        this.artista = "";
-        this.duracion = LocalTime.of(00, 00, 00);
+        this.id= 0 ;
+        this.titulo = "Nueva Cancion";
+        this.artista = "Nuevo Artista";
+        this.duracion = LocalTime.now();
         this.favorita = false;
         this.descargada = false;
     }
+//Getter and Setter
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getTitulo() {
-        return titulo;
+        return this.titulo;
     }
 
     public void setTitulo(String titulo) {
@@ -34,23 +47,28 @@ public class Cancion {
     }
 
     public String getArtista() {
-        return artista;
+        return this.artista;
     }
 
     public void setArtista(String artista) {
-        this.artista = artista;
+        if ( artista.length()>5){
+            this.artista = artista;
+        }
     }
 
     public LocalTime getDuracion() {
-        return duracion;
+        return this.duracion;
     }
 
     public void setDuracion(LocalTime duracion) {
         this.duracion = duracion;
     }
+    public void setDuracion(int minutos, int segundos){
+        this.duracion = duracion;
+    }
 
     public boolean isFavorita() {
-        return favorita;
+        return this.favorita;
     }
 
     public void setFavorita(boolean favorita) {
@@ -58,11 +76,28 @@ public class Cancion {
     }
 
     public boolean isDescargada() {
-        return descargada;
+        return this.descargada;
     }
 
     public void setDescargada(boolean descargada) {
         this.descargada = descargada;
     }
+    //CUSTOM
+    @Override
+    public String toString(){
+        return "Informacion de la canciòn \n" + "-ID: "+this.id + "\n-Titulo: " + this.titulo + "\n-Artista: " 
+                + this.artista + "\n-Duracion: " + this.duracion + "\n-Descagada: " + this.descargada+ "\n-Favorita: "+ this.favorita;
+        
+    }
     
+    public void Adelantar (int SegundosAdelantar){
+       this.duracion = this.duracion.plusSeconds(SegundosAdelantar);
+    }
+    public void Retroceder (int segundoRetroceder){
+       this.duracion = this.duracion.minusSeconds(segundoRetroceder);
+    }
+    
+    private String getDuracionEnMinutos(){
+        return this.duracion.toString();
+    }
 }
