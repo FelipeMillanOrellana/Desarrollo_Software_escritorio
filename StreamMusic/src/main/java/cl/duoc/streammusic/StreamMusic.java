6b8a6fb4e490290fe5ed5090316c67cbccc2a6eb
FelipeMillanOrellana <107309552+FelipeMillanOrellana@users.noreply.github.com>
2024-01-05 21:31:40 -0300

    Update StreamMusic.java

--- StreamMusic/src/main/java/cl/duoc/streammusic/StreamMusic.java
+++ StreamMusic/src/main/java/cl/duoc/streammusic/StreamMusic.java
@@ -1,20 +1,17 @@
 package cl.duoc.streammusic;
 
 import cl.duoc.streammusic.Models.Cancion;
-import java.time.LocalTime;
-
 
 public class StreamMusic {
 
     public static void main(String[] args) {
         Cancion c = new Cancion();
-        
-        c.setArtista("Hola");
-        c.setDuracion(LocalTime.of(00, 03, 30));
+        c.setId(206472);
+        c.setTitulo("Residuos");
+        c.setArtista("El Bestia Negra");
+        c.setDuracion(3,30);
         c.setDescargada(true);
         c.setFavorita(true);
-        
-        
         System.out.println(c);
     }
 }
